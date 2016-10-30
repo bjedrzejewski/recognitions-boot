6cf1d09fd6f406ec7e9183d6ad5319d456e7d1bb
bartoszjedrzejewski <jedrus07@gmail.com>
2016-10-30 19:19:46 +0000

    Initial commit

---
+++ src/main/java/bristech/RecognitionsApplication.java
@@ -0,0 +1,12 @@
+package bristech;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RecognitionsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RecognitionsApplication.class, args);
+	}
+}
---
+++ src/test/java/bristech/RecognitionsApplicationTests.java
@@ -0,0 +1,16 @@
+package bristech;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class RecognitionsApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
